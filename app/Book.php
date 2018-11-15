c5e1f691fb2ef2323e11dc456ba64304547a5d7c
madi-madi <ssss_-_2010@hotmail.com>
2018-11-15 16:35:42 +0200

    factory seed

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $table = 'books';
+    protected $fillable = ['name','details'];
 }
--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Book::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->word,
+        'details'=> $faker->paragraph,
     ];
 });
